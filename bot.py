4cc2b8b384e9f8792274033b3e856c1154782e10
Mimi Sun <47514215+rose8bit@users.noreply.github.com>
2020-05-29 16:04:39 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix = "$", descpription = "I look for food deals on the internet.")	
+bot = commands.Bot(command_prefix = "$", descpription = "I look for food deals on the internet.")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	
 #bot startup
 @bot.event
@@ -19,4 +20,4 @@ async def on_ready():
 	#set "playing" status
 	await bot.change_presence(activity = discord.Game(name = "beep boop"))
 
-bot.run(os.environ.get("BOT_TOKEN"))
+bot.run(BOT_TOKEN)
