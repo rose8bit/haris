c364141163f5add075238ed1bb68c597e1428c86
Mimi Sun <47514215+rose8bit@users.noreply.github.com>
2020-05-29 13:39:03 -0400

    Update greet.py

--- cogs/greet.py
+++ cogs/greet.py
@@ -1,21 +1,27 @@
+"""
+greet.py
+
+created: May 29, 2020
+
+Features:
+- welcomes new members
+
+Commands:
+"""
+
 import discord
 from discord.ext import commands
 
 class Greetings(commands.Cog):
 	def __init__(self, bot):
-		self.client = bot
-		self._last_member = None
+		self.client = bot		
 		
 	@commands.Cog.listener()
 	async def on_member_join(self, member):
 		welcomeMsg = "Welcome {0.mention}!"
 		channel = member.guild.system_channel
 		if channel is not None:
-			await channel.send(welcomeMsg.format(member))
-			
-	@commands.command()
-	async def welcomeMsg(self):
-		await self.client.say(welcomeMsg)
+			await channel.send(welcomeMsg.format(member))			
 		
 def setup(bot):
 	bot.add_cog(Greetings(bot))
