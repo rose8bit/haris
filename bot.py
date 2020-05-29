54559ef5c4cc16c49bc8ff25b71c15430bc41355
Mimi Sun <47514215+rose8bit@users.noreply.github.com>
2020-05-29 14:26:57 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+"""
+bot.py
+Created: May 29, 2020
+"""
+
+#importing modules...
+import discord
+from discord.ext import commands
+import os
+
+#config.py setup
+if not os.path.isfile("config.py"):
+	print("config.py not found")
+else:
+	print("config.py found! importing...")
+	import config
+
+bot = commands.Bot(command_prefix = config.pre, descpription = config.des)	
+	
+#bot startup
+@bot.event
+async def on_ready():
+	print("=============")
+	print("haris is ready!")
+	
+	#set "playing" status
+	await bot.change_presence(activity = discord.Game(name = "beep boop"))
+
+bot.run(config.bot_token)
