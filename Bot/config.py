b285706e398decfcf71911408ce44b66eae68d18
Mimi Sun <47514215+rose8bit@users.noreply.github.com>
2020-05-29 14:05:26 -0400

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,1 +1,13 @@
+#config.py
 
+bot_token = "NzE1NzQ4Mzg1ODMwODYyODY5.XtEI3Q.Dw6HUSMbmtDMJxbRTVOuqy0CTzo"
+
+#extensions
+
+#error messages
+
+#command prefix
+pre='$'
+
+#description
+des = 'haris bot description'
