7a069525132e033cbe000adc4c1bab286d1efd9a
Mimi Sun <47514215+rose8bit@users.noreply.github.com>
2020-06-29 14:25:42 -0400

    Update streamer_role.py

--- cogs/streamer_role.py
+++ cogs/streamer_role.py
@@ -25,7 +25,7 @@ class Streamer_role(commands.Cog):
 		else:
 			print('{} wasn\'t playing anything.'.format(before.display_name))
 		if after.activity is not None:
-			print('{} after activity type: {}\n'.format(after.display_name, str(before.activities)))
+			print('{} after activity type: {}\n'.format(after.display_name, str(after.activities)))
 		else:
 			print('{} has stopped playing.'.format(after.display_name))
 			      
