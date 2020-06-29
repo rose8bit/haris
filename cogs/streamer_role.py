3e63c55556078001793bb0b0d0d5ce9127717e38
Mimi Sun <47514215+rose8bit@users.noreply.github.com>
2020-06-29 10:15:15 -0400

    Update streamer_role.py

--- cogs/streamer_role.py
+++ cogs/streamer_role.py
@@ -5,7 +5,7 @@ created: June 27, 2020
 
 Features:
 - Give members the "LIVE" role when they start streaming if they have the "STREAMER" role
-- Remove the "LIVE" role from members when they stop straeming, if they have the "STREAMER" role
+- Remove the "LIVE" role from members when they stop streaming, if they have the "STREAMER" role
 
 Commands:
 
@@ -18,6 +18,9 @@ class Streamer_role(commands.Cog):
 	def __init__(self, bot):
 		self.client = bot
 		
-
+	@commands.Cog.listener()
+	async def on_member_update(self, before, after):
+		print('{} before activity type: {} \n'.format(before.display_name, before.activity.type)
+		print('{} after activity type: {} \n'.format(after.display_name, after.activity.type)
 def setup(bot):
 	bot.add_Cog(Streamer_role(bot))
